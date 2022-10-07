b17368abf45c4018479a97c014aca5d3d02ec757
AlisonBucker1k <81964399+AlisonBucker1k@users.noreply.github.com>
2022-10-07 18:21:43 -0300

    Cred

--- app/Http/Controllers/DocumentoController.php
+++ app/Http/Controllers/DocumentoController.php
@@ -13,46 +13,49 @@ class DocumentoController extends Controller
 
     public function getFolderByCpf()
     {
-        $docs = Documentos::getAll();
-        $cpfList = [];
+        $request = Http::post(self::$base_api.'token?grant_type=123')->json();
+        dd($request);
 
-        foreach ($docs as $doc) {
-            $dir = explode('/', $doc);
-            $cpf = $dir[2];
+        // $docs = Documentos::getAll();
+        // $cpfList = [];
 
-            if (!in_array($cpf, $cpfList)) {
-                array_push($cpfList, $cpf);
-            }
-        }
+        // foreach ($docs as $doc) {
+        //     $dir = explode('/', $doc);
+        //     $cpf = $dir[2];
 
-        foreach ($cpfList as $cpf) {
-            $folder = Http::post(self::$base_api.'ged-document/document/search', [
-                "filters" => [
-                [
-                    "attributeType" => "METADATA",
-                    "name" => "xnrodocumento",
-                    "value" => $cpf,
-                    "logicOperator" => "AND",
-                    "comparisonOperator" => "EQUAL"
-                ],
-                [
-                    "attributeType" => "DOCUMENT",
-                    "name" => "TYPE_DOCUMENT",
-                    "value" => "TD_PESSOA_VIRTUAL",
-                    "logicOperator" => "AND",
-                    "comparisonOperator" => "EQUAL"
-                ]
-                ],
-                "page" => 0,
-                "searchCriteria" => null,
-                "size" => 1,
-                "sortDirection" => "ASC",
-                "sortField" => [
-                    "CODE"
-                ]
-            ])->json();
+        //     if (!in_array($cpf, $cpfList)) {
+        //         array_push($cpfList, $cpf);
+        //     }
+        // }
 
-            dd($folder);
-        }
+        // foreach ($cpfList as $cpf) {
+        //     $folder = Http::post(self::$base_api.'ged-document/document/search', [
+        //         "filters" => [
+        //         [
+        //             "attributeType" => "METADATA",
+        //             "name" => "xnrodocumento",
+        //             "value" => $cpf,
+        //             "logicOperator" => "AND",
+        //             "comparisonOperator" => "EQUAL"
+        //         ],
+        //         [
+        //             "attributeType" => "DOCUMENT",
+        //             "name" => "TYPE_DOCUMENT",
+        //             "value" => "TD_PESSOA_VIRTUAL",
+        //             "logicOperator" => "AND",
+        //             "comparisonOperator" => "EQUAL"
+        //         ]
+        //         ],
+        //         "page" => 0,
+        //         "searchCriteria" => null,
+        //         "size" => 1,
+        //         "sortDirection" => "ASC",
+        //         "sortField" => [
+        //             "CODE"
+        //         ]
+        //     ])->json();
+
+        //     dd($folder);
+        // }
     }
 }
