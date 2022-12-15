1a9e2df6c2b13dd31b54abeb2b3f27e87572401f
AlisonBucker1k <81964399+AlisonBucker1k@users.noreply.github.com>
2022-12-15 12:49:55 -0300

    without json

--- app/Http/Controllers/DocumentoController.php
+++ app/Http/Controllers/DocumentoController.php
@@ -69,10 +69,10 @@ class DocumentoController extends Controller
         $c = curl_init(self::$base_api.'ged-document/document');
         curl_setopt($c, CURLOPT_POST, 1);
         curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($c, CURLOPT_POSTFIELDS, json_encode($body));
+        curl_setopt($c, CURLOPT_POSTFIELDS, $body);
         curl_setopt($c, CURLOPT_HTTPHEADER, [
             'Host: 15.228.95.130',
-            // 'Authorization: '.$this->getToken(),
+            'Authorization: '.$this->getToken(),
             'Content-Type: application/json',
             'userLoged: sysadmin'
         ]);
