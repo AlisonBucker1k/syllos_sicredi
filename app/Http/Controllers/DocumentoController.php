d1a4339e96bae47b657c557f2cee664a48dcf342
Antonio Bevilaqua <toni_bevila@hotmail.com>
2023-01-05 17:38:28 -0300

    corrigindo request

--- app/Http/Controllers/DocumentoController.php
+++ app/Http/Controllers/DocumentoController.php
@@ -76,7 +76,7 @@ class DocumentoController extends Controller
     {
         $body = $this->getDefaultBody($title);
 
-        $request = Http::withHeaders([
+        $request = Http::dd()->withHeaders([
             'userLogged' => 'sysadmin',
         ])
             ->withoutVerifying()
