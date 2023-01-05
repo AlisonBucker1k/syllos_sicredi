958d3cf41e28d2006d1565719b2219ced0573b6e
Antonio Bevilaqua <toni_bevila@hotmail.com>
2023-01-05 16:47:46 -0300

    corrigindo request

--- app/Http/Controllers/DocumentoController.php
+++ app/Http/Controllers/DocumentoController.php
@@ -124,7 +124,7 @@ class DocumentoController extends Controller
     {
         $body = $this->getDefaultBody($title);
 
-        $request = Http::dd()->withHeaders([
+        $request = Http::withHeaders([
             'Content-Type: application/json',
             'userLogged' => 'sysadmin',
             'enctype' => 'multipart/formdata'
