afd0c7dc90e9e3baf3a2e9c48208813092fe9255
Antonio Bevilaqua <toni_bevila@hotmail.com>
2023-01-09 15:23:47 -0300

    adicionando rota de teste

--- app/Http/Controllers/DocumentoController.php
+++ app/Http/Controllers/DocumentoController.php
@@ -79,8 +79,7 @@ class DocumentoController extends Controller
         $request = Http::dd()->withHeaders([
             'userLogged' => 'sysadmin',
             'Content-Type' => 'application/json'
-        ])
-            ->withoutVerifying()
+        ])->withBody(json_encode(['in' => json_encode($body)]), 'application/json')
             ->withToken($this->getToken())
             ->post(self::$base_api . 'ged-document/document', ['in' => $body]);
 
